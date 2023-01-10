0f9d5638c3606e02fc5ed1455fffef7fb1ecfbd6
MarieKosDuc <marie.koscianski@gmail.com>
2023-01-10 16:36:54 +0100

    intégration du fetch OK

--- background.js
+++ background.js
@@ -3,4 +3,10 @@ console.log("Service worker is active !");
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   console.log(request);
+
+  // Fetching the dictionary API
+  fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + request)
+    .then((response) => response.json())
+    .then((response) => console.log(JSON.stringify(response)))
+    .catch((error) => console.log("Erreur : " + error));
 });
