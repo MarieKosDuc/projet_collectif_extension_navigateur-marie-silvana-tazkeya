0e65610fdd1f8d606561710e8f4bebf8bd69269e
Marie Koscianski-Ducharlet <115166678+MarieKosDuc@users.noreply.github.com>
2023-01-11 16:46:55 +0100

    Suppression fichier de test

--- plop.js
+++
@@ -1,6 +0,0 @@
-console.log("Je suis un fichier de test");
-
-fetch("https://api.dictionaryapi.dev/api/v2/entries/en/blind")
-  .then((response) => response.json())
-  .then((response) => console.log(JSON.stringify(response)))
-  .catch((error) => console.log("Erreur : " + error));
