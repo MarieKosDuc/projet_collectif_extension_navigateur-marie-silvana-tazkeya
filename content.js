d19e16c15246e9a740a40917e4ca74671fe37427
MarieKosDuc <marie.koscianski@gmail.com>
2023-01-16 11:44:01 +0100

    Fix bug break line when multiples definitions

--- content.js
+++ content.js
@@ -2,9 +2,19 @@ console.log("Content.js active!");
 
 // Creating a div that will contain the definitions
 function createDiv() {
+  // creating a container div
   const injectElement = document.createElement("div");
   injectElement.setAttribute("id", "definitionDiv");
   injectElement.className = "Word definition";
+
+  // creating a content div that will contain the definition
+  const injectTextContentDiv = document.createElement("div");
+  injectTextContentDiv.setAttribute("id", "textHere");
+
+  // adding the content text to the container
+  injectElement.appendChild(injectTextContentDiv);
+
+  // adding the definitions div to the page's body
   document.body.appendChild(injectElement);
 }
 
@@ -31,22 +41,27 @@ function getDefinition(word) {
 function setValues(returnedArray) {
   // getting the definitions by grammatical gategory
   let wordDefinitions = [];
-  let simplifiedArray = returnedArray[0].meanings;
-  for (i = 0; i < simplifiedArray.length; i++) {
-    category = simplifiedArray[i].partOfSpeech;
-    wordDef = simplifiedArray[i].definitions[0].definition;
+  if (returnedArray.title == "No Definitions Found") {
+    wordDefinitions.push("Sorry, no definition found");
+  } else {
+    let simplifiedArray = returnedArray[0].meanings;
+    for (i = 0; i < simplifiedArray.length; i++) {
+      category = simplifiedArray[i].partOfSpeech;
+      wordDef = simplifiedArray[i].definitions[0].definition;
 
-    // pushing it into an array
-    wordDefinitions.push(category + ": " + wordDef);
+      // pushing it into an array
+      wordDefinitions.push(category + ": " + wordDef + "<br />");
+    }
   }
   console.log(wordDefinitions);
 
   // creating HTML
-  setDef = document.getElementById("definitionDiv");
-  setDef.innerText = wordDefinitions;
+  setDef = document.getElementById("textHere");
+  setDef.innerHTML = wordDefinitions;
 
-  chrome.scripting.insertCSS({
-    files: ["style.css"],
-    target: { tabId: tab.id },
-  });
+  // insert CSS - not working
+  // chrome.scripting.insertCSS({
+  //   files: ["style.css"],
+  //   target: { tabId: tab.id },
+  // });
 }
