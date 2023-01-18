42916a2f16be6cbef5acc396614b0515c4f2c20a
tazkeyaislam <114977400+tazkeyaislam@users.noreply.github.com>
2023-01-18 11:35:19 +0100

    dictionary added on the top left corner!

---
+++ ce.js
@@ -0,0 +1,25 @@
+// constgetElementsByClassName(Aide)
+const url= "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result= document.getElementById("result");
+const sound= document.getElementById("sound");
+
+const btn= document.getElementById("search-btn");
+
+btn.addEventListener("click",() => {
+    let inpWord = document.getElementById("input-word").value;
+    fetch(`${url}${inpWord}`)
+    .then((response) => response.json())
+    .then((data) => {console.log(data);
+    result.innerHTML=`
+    <div class="word">
+    <h3>${inpWord}</h3>
+</div>
+<div class="details">
+    <p>${data[0].meanings[0].partOfSpeech}</p>
+    <p>/${data[0].phonetic}/</p>
+</div>
+<p class="wordDef">
+    ${data[0].meanings[0].definitions[0].definition}  
+</p>`;   
+});
+});
